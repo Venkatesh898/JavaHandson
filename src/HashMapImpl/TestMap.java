ebd48624ad879d0cc03eaf2aaa5be252af68c801
Venkatesh <venkypoluri898@gmail.com>
2024-10-08 20:18:53 +0530

    Implementation of HashMap using linked list in java from scratch and considering rehashing scenario as well.

---
+++ src/HashMapImpl/TestMap.java
@@ -0,0 +1,18 @@
+package HashMapImpl;
+
+import java.util.Map;
+
+public class TestMap {
+    public static void main(String[] args) {
+        OurMap<String,Integer> map=new OurMap<>();
+        map.put("ram",2);
+        map.put("mohan",3);
+        map.put("anil",8);
+        map.put("madan",90);
+        map.put("sam",89);
+        map.put("roy",89);
+        map.put("tom",89);
+        map.put("ben",89);
+
+    }
+}
